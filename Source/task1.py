d00f47095eca27d062351f28db6fb7a3008abfeb
Maksim <makc.pinsk@gmail.com>
2020-01-27 17:48:51 +0300

    task1

---
+++ Source/task1.py
@@ -0,0 +1,21 @@
+import re
+
+
+def task1():
+    numbers = []
+    regex_private = r'[АВЕКМНОРСТУХ]\d{3}[АВЕКМНОРСТУХ]{2}\d{2,3}'
+    regex_taxi = r'[АВЕКМНОРСТУХ]{2}\d{3}\d{2,3}'
+    while True:
+        number = input()
+        if number == '':
+            break
+        else:
+            numbers.append(number)
+    for num in numbers:
+        if re.fullmatch(regex_private, num) != None:
+            print(num, ':Private')
+        elif re.fullmatch(regex_taxi, num) != None:
+            print(num, ':Taxi')
+        else:
+            print(num, ':Fail')
+task1()
