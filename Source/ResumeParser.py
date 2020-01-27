0e4355d2ee8e8c4f3071b08a096e8c14cf41348d
siarhei kalashynski <sergei.kalashynski@gmail.com>
2020-01-27 12:25:38 +0300

    code review

--- Source/ResumeParser.py
+++ Source/ResumeParser.py
@@ -1,5 +1,5 @@
 import re, io
-from Source.pdfProcesser import output_path, extract_txt_from_pdf
+from Source.pdfProcesser import get_output_path, parse_pdf
 
 
 # создать класс user
@@ -11,23 +11,24 @@ class User():
         self.mobile_phone = mobile_phone
         self.name = name
 
-    def userInformation(self):
+    def to_string(self):
         return self.name, self.mobile_phone, self.email
 
 
-def findUser():
-    pdf_path = r'..\Recourse\Input\resume.pdf'
-    with io.open(output_path(pdf_path), 'w+', encoding="utf-8") as file:
-        text = extract_txt_from_pdf(pdf_path)
-        regex_name = (r'\w+\s\w+\s\w+')
-        regex_email = (r'\w+\.\w+@\w+.com')
-        regex_mobile_phone = (r'[+]\d{12}')
+def parser_resume(resume_path):
+    regex_name = (r'\w+\s\w+\s\w+')
+    regex_email = (r'\w+\.\w+@\w+.com')
+    regex_mobile_phone = (r'[+]\d{12}')
+
+    with io.open(get_output_path(pdf_path), 'w+', encoding="utf-8") as file:
+        text = parse_pdf(resume_path)
         email = re.findall(regex_email, text)[0]
         mobile_phone = re.findall(regex_mobile_phone, text)[0]
         name = re.findall(regex_name, text)[0]
         return email, mobile_phone, name
 
 
-data = findUser()
+pdf_path = r'..\Recourse\Input\resume.pdf'
+data = parser_resume(pdf_path)
 maksim = User(data[0], data[1], data[2])
-print(maksim.userInformation())
+print(maksim.to_string())
--- Source/pdfProcesser.py
+++ Source/pdfProcesser.py
@@ -5,25 +5,25 @@ from pdfminer.pdfinterp import PDFPageInterpreter, PDFResourceManager
 from pdfminer.pdfpage import PDFPage
 
 
-def extract_txt_from_pdf(pdf_path):
+def parse_pdf(pdf_path):
     resource_manager = PDFResourceManager()  # используется для хранения общих ресурсов, таких как шрифты или изображения.(Создаем экземпляр ресурса менеджера)
-    take_file_handle = io.StringIO()  # создаем файловый объект
-    converter = TextConverter(resource_manager, take_file_handle)  # (создание конвертера)
+    input_stream = io.StringIO()  # создаем файловый объект
+    converter = TextConverter(resource_manager, input_stream)  # (создание конвертера)
     page_interpreter = PDFPageInterpreter(resource_manager,
                                           converter)  # создаем объект интерпритаторов который извлечет текст
     with open(pdf_path, 'rb') as file:
         for page in PDFPage.get_pages(file, caching=True, check_extractable=True):
             page_interpreter.process_page(page)
-        text = take_file_handle.getvalue()
+        text = input_stream.getvalue()
     converter.close()
-    take_file_handle.close()
+    input_stream.close()
 
     if text:
         return text
 
 
-def output_path(pdf_path):
-    full_name = path.basename(pdf_path)
+def get_output_path(resource_path):
+    full_name = path.basename(resource_path)
     name = path.splitext(full_name)[0]
     way = "..\Recourse\Output\\" + name + ".txt"
     return way
