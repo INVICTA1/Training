b4a68ddf02e3e90caba7bd3d8bfb0c694cfa53dc
Maksim <makc.pinsk@gmail.com>
2020-01-28 23:56:23 +0300

    parsing xml

--- Source/xmlProcesser.py
+++ Source/xmlProcesser.py
@@ -4,4 +4,7 @@ regex='\w{3}'
 tree = ET.parse('..\Recourse\Input\XML.xml')
 root = tree.getroot()
 print(root.attrib)
-print(root)
+for child in root:
+    print(child.tag,child.attrib)
+    for child2 in child:
+        print(child2.tag)
