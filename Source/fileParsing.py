153baf2cb3ed861a714f42c1147b18e010d38433
Maksim <makc.pinsk@gmail.com>
2020-02-02 15:03:12 +0300

    fix name

--- Source/fileParsing.py
+++ Source/fileParsing.py
@@ -1,10 +1,10 @@
 from Source.xmlProcesser import *
 from Source.Fw9Parser import *
-from Source.tasks import task2_find_emails,output_emails_in_file
+from Source.tasks import output_emails_in_file
 from os import path, listdir
 
 
-def parsing_file(path_files):
+def parsing_directory(path_files):
     files = listdir(path_files)
     fun = {'.pdf': splitting_text_into_sentences, '.xml': output_data_in_file, '.txt': output_emails_in_file}
     for file in files:
@@ -17,4 +17,4 @@ def parsing_file(path_files):
 
 if __name__ == '__main__':
     path_files = '../Recourse/Input/'
-    parsing_file(path_files)
+    parsing_directory(path_files)
